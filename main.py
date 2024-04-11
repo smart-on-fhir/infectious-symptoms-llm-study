8b05d30d53f6356fbb0b75adb63fd639723881a9
Dylan Phelan <Dtphelan1@gmail.com>
2024-04-11 08:51:22 -0400

    main now just prints prompts

--- main.py
+++ main.py
@@ -1,77 +1,75 @@
+import requests
 import re
 import os
-from dotenv import load_dotenv
-from src.strategy import Strategy
+import json
+import random
+import datetime
+from functools import reduce 
+
+## Custom methods
+from src.processor import NoteProcessor
 from src.model_interface import LLAMA2Interface
-from src.processor import process_small_batch, process_dir
-from src.instructions import simple_instruction
+from src.covid_study_strategies import build_strategies
+
 
 ###############################################################################
 #
 # Use LLAMA2 as our interface
 # 
-load_dotenv()
-URL=f"{os.environ['HOST']}:{os.environ['PORT']}/"
-llama2 = LLAMA2Interface(URL)
+# Make sure you check the info printed later to be sure you are on the right port
+HOST="http://localhost"
+PORT='8086'
+URL=f"{HOST}:{PORT}/"
 
+noteConfig = { 
+    "DIR_OUTPUT_TUNING": "/lab-share/CHIP-Mandl-e2/Public/covid-llm/output-prompt-tuning",
+    "DIR_OUTPUT": "/lab-share/CHIP-Mandl-e2/Public/covid-llm/output-pipeline-analysis-i2b2",
+    "DIR_INPUT": '/lab-share/CHIP-Mandl-e2/Public/covid-llm/notes-original-study/i2b2',
+}
+note_processor = NoteProcessor(noteConfig)
+model = LLAMA2Interface(URL)
 
-###############################################################################
-#
-# Custom preprocessor: whitespace normalization
-# 
-# Find all instances of multiple white space repeated
-pattern = r"(\s){2,}"
-# Replace it with one instance via capture group
-replacement = r"\1"
-def whitespace_normalize(s: str): 
-    return re.sub(pattern, replacement, s).strip()
-
-
-###############################################################################
-#
-# Simplification second-pass
-# 
-simplify_instruction = """
-You are an expert editor reviewing a clinical note summary. 
-Previous reviewer may have included irrelevant, negative symptoms in their summarization. 
-
-Remove mentions of negative symptoms from this summary (e.g. "No X, No recent Y, No recent changes in Z").
-ONLY reply with the summary. 
-Do NOT explain your answers.
-"""
-
-###############################################################################
-#
-# Defining strategies with the components above!
-# 
-simpleDoublePassStrategy = Strategy([
-    {
-        "instruction": simple_instruction(),
-        "preprocess": whitespace_normalize,
-    },
-    {
-        "step_type": "previous",
-        "instruction": simplify_instruction,
-        "preprocess": whitespace_normalize,
-    }
-], model=llama2)
-simpleSinglePassStrategy = Strategy([
-    {
-        "instruction": simple_instruction(),
-        "preprocess": whitespace_normalize,
-    }
-], model=llama2)
+def list_experiment_strategies(exp): 
+    return reduce(lambda k1, k2: k1 + ', ' + k2, exp.keys())
 
 
 ###############################################################################
 #
 # Building experiment with strategies 
-# 
-experiment = { 
-  "simpleDoublePassStrategy": simpleDoublePassStrategy,
-  "simpleSinglePassStrategy": simpleSinglePassStrategy,
+#
+all_strategies = build_strategies(model) 
+exp = {
+    "prompt-llama2Simple":   all_strategies["simple"],
+    "prompt-llama2Identity": all_strategies["identity"],
+    "prompt-llama2Include":  all_strategies["include"],
+    "prompt-llama2Exclude":  all_strategies["exclude"],
+    "prompt-llama2Verbose":  all_strategies["verbose"],
+    # DoublePass
+    "prompt-llama2SimpleDoublePass":   all_strategies["simpleDoublePass"],
+    "prompt-llama2IdentityDoublePass": all_strategies["identityDoublePass"],
+    "prompt-llama2IncludeDoublePass":  all_strategies["includeDoublePass"],
+    "prompt-llama2ExcludeDoublePass":  all_strategies["excludeDoublePass"],
+    "prompt-llama2VerboseDoublePass":  all_strategies["verboseDoublePass"],
+    # JSON
+    "prompt-llama2SimpleJSON":   all_strategies["simpleJSON"],
+    "prompt-llama2IdentityJSON": all_strategies["identityJSON"],
+    "prompt-llama2IncludeJSON":  all_strategies["includeJSON"],
+    "prompt-llama2ExcludeJSON":  all_strategies["excludeJSON"],
+    "prompt-llama2VerboseJSON":  all_strategies["verboseJSON"],
+    # JSON Double Pass
+    "prompt-llama2SimpleJSONDoublePass":   all_strategies["simpleJSONDoublePass"], 
+    "prompt-llama2IdentityJSONDoublePass": all_strategies["identityJSONDoublePass"],
+    "prompt-llama2IncludeJSONDoublePass":  all_strategies["includeJSONDoublePass"],
+    "prompt-llama2ExcludeJSONDoublePass":  all_strategies["excludeJSONDoublePass"],
+    "prompt-llama2VerboseJSONDoublePass":  all_strategies["verboseJSONDoublePass"],
 }
 
+list_experiment_strategies(exp)
+
 if __name__ == "__main__":
-  # process_small_batch(experiment=experiment)
-  process_dir(experiment=experiment)
+    for k,v in exp.items(): 
+        print('----------------------------')
+        print(k)
+        for s in v.steps:
+            print('# step')
+            print(s.instruction)
